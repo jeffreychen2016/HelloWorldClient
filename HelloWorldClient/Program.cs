508eb892f3acc27c2042f79860a9dd191203df65
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-10-10 16:29:13 -0500

    create sample WCF client

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HelloWorldService.HelloWorldServiceClient();
+            var helloFromService = client.HelloWorld();
+            Console.WriteLine(helloFromService);
+        }
+    }
+}
